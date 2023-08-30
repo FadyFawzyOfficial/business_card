44f2d8c511d57ef52ee1f484403486b406870f31
Fady Fawzy <FadyFawzyOfficial@gmail.com>
2023-08-30 12:32:55 +0300

    5. Wrap the Screen with SafeArea Widget & Finish it

--- lib/main.dart
+++ lib/main.dart
@@ -8,6 +8,7 @@ class BusinessCard extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       theme: ThemeData(
         cardTheme: const CardTheme(
           margin: EdgeInsets.zero,
@@ -18,68 +19,73 @@ class BusinessCard extends StatelessWidget {
       ),
       home: const Scaffold(
         backgroundColor: Colors.blueGrey,
-        body: Padding(
-          padding: EdgeInsets.symmetric(horizontal: 16),
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              CircleAvatar(
-                radius: 101,
-                backgroundColor: Colors.white,
-                child: CircleAvatar(
-                  radius: 100,
-                  backgroundImage: AssetImage('assets/images/fady.jpg'),
+        body: SafeArea(
+          child: Padding(
+            padding: EdgeInsets.symmetric(horizontal: 16),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                CircleAvatar(
+                  radius: 101,
+                  backgroundColor: Colors.white,
+                  child: CircleAvatar(
+                    radius: 100,
+                    backgroundImage: AssetImage('assets/images/fady.jpg'),
+                  ),
                 ),
-              ),
-              Text(
-                'Fady Fawzy',
-                style: TextStyle(
-                  fontSize: 32,
-                  color: Colors.white,
-                  fontFamily: 'Pacifico',
+                Text(
+                  'Fady Fawzy',
+                  style: TextStyle(
+                    fontSize: 32,
+                    color: Colors.white,
+                    fontFamily: 'Pacifico',
+                  ),
                 ),
-              ),
-              Text(
-                'SOFTWARE ENGINEER',
-                style: TextStyle(
-                  fontSize: 18,
+                Text(
+                  'SOFTWARE ENGINEER',
+                  style: TextStyle(
+                    fontSize: 18,
+                    color: Colors.white54,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+                Divider(
+                  height: 32,
                   color: Colors.white54,
-                  fontWeight: FontWeight.bold,
                 ),
-              ),
-              Divider(color: Colors.white54),
-              Card(
-                child: ListTile(
-                  leading: Icon(
-                    Icons.phone_rounded,
-                    color: Colors.blueGrey,
-                  ),
-                  title: Text(
-                    '+2 012 878 111 76',
-                    style: TextStyle(
-                      fontSize: 16,
+                Card(
+                  child: ListTile(
+                    leading: Icon(
+                      Icons.phone_rounded,
                       color: Colors.blueGrey,
+                    ),
+                    title: Text(
+                      '+2 012 878 111 76',
+                      style: TextStyle(
+                        fontSize: 16,
+                        color: Colors.blueGrey,
+                      ),
                     ),
                   ),
                 ),
-              ),
-              SizedBox(height: 16),
-              Card(
-                child: ListTile(
-                  leading: Icon(
-                    Icons.email_rounded,
-                    color: Colors.blueGrey,
-                  ),
-                  title: Text(
-                    'FadyFawzy.Official@gmail.com',
-                    style: TextStyle(
-                      fontSize: 16,
+                SizedBox(height: 16),
+                Card(
+                  child: ListTile(
+                    leading: Icon(
+                      Icons.email_rounded,
                       color: Colors.blueGrey,
+                    ),
+                    title: Text(
+                      'FadyFawzy.Official@gmail.com',
+                      style: TextStyle(
+                        fontSize: 16,
+                        color: Colors.blueGrey,
+                      ),
                     ),
                   ),
                 ),
-              ),
-            ],
+              ],
+            ),
           ),
         ),
       ),
