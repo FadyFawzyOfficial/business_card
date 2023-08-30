9d054f61bd7ddf5b2cf17f0be08c132994fd2964
Fady Fawzy <FadyFawzyOfficial@gmail.com>
2023-08-30 12:10:28 +0300

    3. Use Card and ListTile Widgets

--- lib/main.dart
+++ lib/main.dart
@@ -7,30 +7,78 @@ class BusinessCard extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: Scaffold(
+    return MaterialApp(
+      theme: ThemeData(
+        cardTheme: const CardTheme(
+          margin: EdgeInsets.zero,
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.all(Radius.circular(8)),
+          ),
+        ),
+      ),
+      home: const Scaffold(
         backgroundColor: Colors.blueGrey,
-        body: Column(
-          crossAxisAlignment: CrossAxisAlignment.stretch,
-          children: [
-            CircleAvatar(
-              radius: 101,
-              backgroundColor: Colors.white,
-              child: CircleAvatar(
-                radius: 100,
-                backgroundImage: AssetImage('assets/images/fady.jpg'),
+        body: Padding(
+          padding: EdgeInsets.symmetric(horizontal: 16),
+          child: Column(
+            children: [
+              CircleAvatar(
+                radius: 101,
+                backgroundColor: Colors.white,
+                child: CircleAvatar(
+                  radius: 100,
+                  backgroundImage: AssetImage('assets/images/fady.jpg'),
+                ),
               ),
-            ),
-            Text(
-              'Fady Fawzy',
-              textAlign: TextAlign.center,
-              style: TextStyle(
-                fontSize: 32,
-                color: Colors.white,
-                fontFamily: 'Pacifico',
+              Text(
+                'Fady Fawzy',
+                style: TextStyle(
+                  fontSize: 32,
+                  color: Colors.white,
+                  fontFamily: 'Pacifico',
+                ),
               ),
-            ),
-          ],
+              Text(
+                'SOFTWARE ENGINEER',
+                style: TextStyle(
+                  fontSize: 18,
+                  color: Colors.white38,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              Card(
+                child: ListTile(
+                  leading: Icon(
+                    Icons.phone_rounded,
+                    color: Colors.blueGrey,
+                  ),
+                  title: Text(
+                    '+2 012 878 111 76',
+                    style: TextStyle(
+                      fontSize: 16,
+                      color: Colors.blueGrey,
+                    ),
+                  ),
+                ),
+              ),
+              SizedBox(height: 16),
+              Card(
+                child: ListTile(
+                  leading: Icon(
+                    Icons.email_rounded,
+                    color: Colors.blueGrey,
+                  ),
+                  title: Text(
+                    'FadyFawzy.Official@gmail.com',
+                    style: TextStyle(
+                      fontSize: 16,
+                      color: Colors.blueGrey,
+                    ),
+                  ),
+                ),
+              ),
+            ],
+          ),
         ),
       ),
     );
